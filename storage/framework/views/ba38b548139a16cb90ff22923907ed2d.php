845aa1527664afa62903365ef41be8f63685d8cd
Harris <acessonly11@gmail.com>
2025-02-19 20:24:33 +0500

    g

--- storage/framework/views/ba38b548139a16cb90ff22923907ed2d.php
+++ storage/framework/views/ba38b548139a16cb90ff22923907ed2d.php
@@ -186,11 +186,21 @@
                     <?php if(Auth::user()->userRole->name == 'Admin' || in_array('147', $phoneaccess)): ?>
                         <li>
                             <a class="side-menu__item" href="<?php echo e(route('view_query')); ?>">
-                                <span class="js-search-result-thumbnail responsive-img img_border fa fa-question"></span>
+                                <span
+                                    class="js-search-result-thumbnail responsive-img img_border fa fa-question"></span>
                                 <span class="side-menu__label">Shipa1 Query</span>
                             </a>
                         </li>
                     <?php endif; ?>
+                    
+                        <li>
+                            <a class="side-menu__item" href="<?php echo e(route('links')); ?>">
+                                <span
+                                    class="js-search-result-thumbnail responsive-img img_border fa fa-question"></span>
+                                <span class="side-menu__label">Links</span>
+                            </a>
+                        </li>
+                    
                     <?php if(in_array('0', $phoneaccess)): ?>
                         <li>
                             <a class="side-menu__item" href="<?php echo e(url('new')); ?>">
@@ -816,7 +826,7 @@
                     <?php if(Auth::user()->userRole->name == 'Admin' || in_array('137', $phoneaccess)): ?>
                         <li>
                             <a style="background: #2dd1ba8c" class="side-menu__item"
-                               href="<?php echo e(url('autos_approach_new1') . '/' . base64_encode('Shipper')); ?>">
+                                href="<?php echo e(url('autos_approach_new1') . '/' . base64_encode('Shipper')); ?>">
                                 <span class="js-search-result-thumbnail responsive-img img_border fa fa-mobile"></span>
                                 <span class="side-menu__label">DayDispatch | Shipper</span>
                                 <span class="badge badge-warning side-badge" style="font-size: 15px"></span>
@@ -826,7 +836,7 @@
                     <?php if(Auth::user()->userRole->name == 'Admin' || in_array('138', $phoneaccess)): ?>
                         <li>
                             <a style="background: #2dd1ba8c" class="side-menu__item"
-                               href="<?php echo e(url('autos_approach_new1') . '/' . base64_encode('Carrier')); ?>">
+                                href="<?php echo e(url('autos_approach_new1') . '/' . base64_encode('Carrier')); ?>">
                                 <span class="js-search-result-thumbnail responsive-img img_border fa fa-mobile"></span>
                                 <span class="side-menu__label">DayDispatch | Carrier </span>
                                 <span class="badge badge-warning side-badge" style="font-size: 15px"></span>
@@ -836,18 +846,17 @@
                     <?php if(Auth::user()->userRole->name == 'Admin' || in_array('139', $phoneaccess)): ?>
                         <li>
                             <a style="background: #2dd1ba8c" class="side-menu__item"
-                               href="<?php echo e(url('autos_approach_new1') . '/' . base64_encode('Broker')); ?>">
+                                href="<?php echo e(url('autos_approach_new1') . '/' . base64_encode('Broker')); ?>">
                                 <span class="js-search-result-thumbnail responsive-img img_border fa fa-mobile"></span>
                                 <span class="side-menu__label">DayDispatch | Broker </span>
                                 <span class="badge badge-warning side-badge" style="font-size: 15px"></span>
                             </a>
                         </li>
-
                     <?php endif; ?>
                     <?php if(Auth::user()->userRole->name == 'Admin' || in_array('140', $phoneaccess)): ?>
                         <li>
                             <a style="background: #2dd1ba8c" class="side-menu__item"
-                               href="<?php echo e(route('autos.autos_approach_new_dealer')); ?>">
+                                href="<?php echo e(route('autos.autos_approach_new_dealer')); ?>">
                                 <span class="js-search-result-thumbnail responsive-img img_border fa fa-mobile"></span>
                                 <span class="side-menu__label">Dealer Approaching</span>
                                 <span class="badge badge-warning side-badge" style="font-size: 15px"></span>
